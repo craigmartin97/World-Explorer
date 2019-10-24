b1e24b85e8d99d199e52b427d2b868f4972c6664
craigmartin97 <craigmartin2013@yahoo.co.uk>
2019-10-24 10:38:57 +0100

    Added additional tests to tuple size compare

--- src/tuple_size_compare/core.clj
+++ src/tuple_size_compare/core.clj
@@ -206,7 +206,10 @@
                   (in ?obj ?room1)
                   (holds ?agent ??x))
             :add ((holds ?agent ??x ?obj))
-            :del ((holds ?agent ??x))
+            :del (
+                   (in ?obj ?room1)
+                   (holds ?agent ??x)
+                   )
             :txt (?agent picked up ?obj from ?room1)
             }
     drop {:pre ((agent ?agent)
@@ -306,6 +309,98 @@
 
      (holdable watch)
      (in watch G)
+
+     }
+  )
+
+(def state-huge
+  '#{
+     ;define agentLL
+     (agent R)
+     ;define rooms
+     (room A)
+     (room B)
+     (room C)
+     (room D)
+     (room E)
+     (room F)
+     (room G)
+     (room H)
+     (room I)
+     (room J)
+     (room K)
+
+     (in R A)
+     (holds R)
+
+     (holdable key)
+     (in key D)
+
+     (holdable dog)
+     (in dog A)
+
+     (holdable cat)
+     (in cat A)
+
+     (holdable mouse)
+     (in mouse A)
+
+     (holdable phone)
+     (in phone B)
+
+     (holdable cup)
+     (in cup F)
+
+     (holdable glass)
+     (in glass K)
+
+     (holdable remote)
+     (in remote I)
+
+     (holdable water)
+     (in water I)
+
+     (holdable watch)
+     (in watch G)
+
+     (holdable case)
+     (in case E)
+
+     (holdable keyboard)
+     (in keyboard K)
+
+     (holdable screen)
+     (in screen J)
+
+     (holdable chair)
+     (in chair A)
+
+     (holdable folder)
+     (in folder D)
+
+     (holdable apple)
+     (in apple D)
+
+     (holdable mango)
+     (in mango G)
+
+     (holdable pear)
+     (in pear H)
+
+     (holdable glove)
+     (in glove I)
+
+     (holdable hat)
+     (in hat B)
+
+     (holdable scarf)
+     (in scarf C)
+
+     (holdable tea)
+     (in tea C)
+
+     (holdable coffee)
+     (in coffee D)
 
      }
   )
@@ -443,8 +538,13 @@
   A duplicate dog object has been specified in the goal state
   to see how the ops-search will react
 
-  Ops-search will pick up the same object multiple times
-  Elapsed time: 13039.4086 msecs"
+  Op-search will try and pick up one dog, and another and another.
+  As there is only one dog in the world the agent wont be able to do this.
+  It will manage to find one dog, and try every possible combination and explore all
+  depths until it eventually fails and returns nil.
+
+  The ide in this case will run out of avaliable memory before this happens.
+  Elapsed time: unable to solve"
   (time (ops-search state '((holds R dog cat mouse key dog)) operations))
   )
 
@@ -454,7 +554,7 @@
   to see how the ops-search will react
 
   Ops-search will pick up the same object multiple times
-  Elapsed time: 13039.4086 msecs"
+  Elapsed time: unalbe to solve"
   (time (ops-search state '((holds R dog dog dog)) operations))
   )
 
@@ -476,18 +576,126 @@
 ;----------------------------------
 ;----------------------------------
 
-(defn ops-pickup-many-items []
-  "Agent picks up dog, cat and mouse using ops-search
 
-  Elapsed time: 25.3425 msecs"
-  (time (ops-search state '((holds R key)) operations))
+;--------------------------------
+;---------------Pickup 6 items-----------------
+;--------------------------------
+
+
+(defn ops-search-pickup-six-objs []
+  "
+  Agent picksup 6 objects using ops-search
+  Elapsed time: "
+  (time (ops-search state-many-objs '((holds R key dog cat mouse phone cup)) operations :debug true))
   )
 
-(defn planner-pickup-many-items []
-  "Agent picks up the dog, cat and mouse from room A using planner
+(defn planner-search-pickup-six-objs []
+  "
+  Agent picksup 6 objects using planner
 
-  Elapsed time:4.0412 msecs"
-  (time (planner state '(holds R key) planner-operations))
+  Elapsed time: 67.8264 msecs"
+  (time (planner state-many-objs '(holds R  key dog cat mouse phone cup) planner-operations))
   )
 
-;;TODO - Add more tests with the agent picking up more items
+;-----------------------------------
+;-----------------------------------
+;-----------------Pickup 8 items------------------
+;-----------------------------------
+;-----------------------------------
+
+
+
+(defn ops-search-pickup-eight-objs []
+  "
+  Agent will pickup eight objects using op-search
+  Elapsed time: "
+  (time (ops-search state-many-objs '((holds R key dog cat mouse phone cup glass remote)) operations :debug true))
+  )
+
+(defn planner-search-pickup-eight-objs []
+  "
+  Agent picks up eight objects using planner
+
+  Elapsed time: 67.8264 msecs"
+  (time (planner state-many-objs '(holds R  key dog cat mouse phone cup glass remote) planner-operations))
+  )
+
+;------------------------------------
+;------------------------------------
+;---------------Pickup ten items---------------------
+;------------------------------------
+;------------------------------------
+
+(defn ops-search-pickup-many []
+  "
+  Agent picks up many objects (10) using ops-search
+  Elapsed time: unsolvable"
+  (time (ops-search state-many-objs '((holds R key dog cat mouse phone cup glass remote water watch)) operations :debug true))
+  )
+
+(defn planner-search-pickup-many []
+  "
+  Agent picks up many objects (10) using planner
+
+  Elapsed time: 67.8264 msecs"
+  (time (planner state-many-objs '(holds R  key dog cat mouse phone cup glass remote water watch) planner-operations))
+  )
+
+;-------------------------------
+;-------------------------------
+;---------------Planner with huge state----------------
+;-------------------------------
+;-------------------------------
+
+(defn planner-search-pickup-huge-state-13 []
+     "
+     Agent picks up lots of items from different locations using planner
+
+     Elapsed time: 117.7888 msecs"
+     (time (planner state-huge '(holds R  key dog cat mouse phone cup glass remote water watch
+                                       case keyboard screen
+                                       ) planner-operations))
+     )
+
+(defn planner-search-pickup-huge-state-14 []
+  "
+  Agent picks up lots of items from different locations using planner
+
+  Elapsed time: 120.6767 msecs"
+  (time (planner state-huge '(holds R  key dog cat mouse phone cup glass remote water watch
+                                    case keyboard screen chair
+                                    ) planner-operations))
+  )
+
+(defn planner-search-pickup-huge-state-15 []
+  "
+  Agent picks up lots of items from different locations using planner
+
+  Elapsed time: 134.023 msecs"
+  (time (planner state-huge '(holds R  key dog cat mouse phone cup glass remote water watch
+                                    case keyboard screen chair folder
+                                    ) planner-operations))
+  )
+
+(defn planner-search-pickup-huge-state-16 []
+  "
+  Agent picks up lots of items from different locations using planner
+
+  Elapsted time (when limit was 100) : 167.133399 msecs
+  Elapsed time: unsolvable, unless we edit the limit in planner code (60 to 100)"
+  (time (planner state-huge '(holds R  key dog cat mouse phone cup glass remote water watch
+                                    case keyboard screen chair folder apple
+                                    ) planner-operations))
+  )
+
+(defn planner-search-pickup-huge-state-all []
+  "
+  Agent picks up lots of items from different locations using planner
+
+  Elapsted time (when limit was 120) : 307.8745 msecs
+  Elapsed time: unsolvable, unless we edit the limit in planner code (60 to 120)"
+  (time (planner state-huge '(holds R  key dog cat mouse phone cup glass remote water watch
+                                    case keyboard screen chair folder apple mango
+                                    pear glove hat scarf tea coffee
+                                    ) planner-operations))
+  )
