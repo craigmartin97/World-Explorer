456e1acb1bca7f8962f580bebe0aafc31975256a
MCBURNEY <S6042911@live.tees.ac.uk>
2019-10-28 14:32:13 +0000

    Update base state size to incorporate planner

--- src/foo/handler.clj
+++ src/foo/handler.clj
@@ -1,24 +1,24 @@
-(ns foo.handler
-  (:gen-class) ;; <--- you need this too!
-  (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
-            [compojure.route :as route]
-            [ring.adapter.jetty :refer :all] ;; <-- need a server!
-            [tuple-size.one :refer :all]
-            [tuple-size.eleven :refer :all]
-            ))
-
-;; Compojure routes
-(defroutes app-routes
-           (GET "/" ... ))
-
-;; Ring handler
-(def app
-  (handler/site app-routes))
-
-;; Pass the handler to Jetty on port 8080
-(defn -main []
-
-  (test-one-move-to-f)
-  ; (test-eleven-move-to-f)
-  )
+;(ns foo.handler
+;  (:gen-class) ;; <--- you need this too!
+;  (:require [compojure.core :refer :all]
+;            [compojure.handler :as handler]
+;            [compojure.route :as route]
+;            [ring.adapter.jetty :refer :all] ;; <-- need a server!
+;            [tuple-size.one :refer :all]
+;            [tuple-size.eleven :refer :all]
+;            ))
+;
+;;; Compojure routes
+;(defroutes app-routes
+;           (GET "/" ... ))
+;
+;;; Ring handler
+;(def app
+;  (handler/site app-routes))
+;
+;;; Pass the handler to Jetty on port 8080
+;(defn -main []
+;
+;  (test-one-move-to-f)
+;  ; (test-eleven-move-to-f)
+;  )
